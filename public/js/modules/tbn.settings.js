8835acccb540a114cb138715323f46ad609fff25
Alexis Jacomy <alexis.jacomy@gmail.com>
2014-04-04 17:09:09 +0200

    Started implementing the settings view + module

--- public/js/modules/tbn.settings.js
+++ public/js/modules/tbn.settings.js
@@ -2,9 +2,28 @@
   'use strict';
 
   tbn.pkg('tbn.modules');
-  tbn.modules.settings = function(dom) {
+  tbn.modules.settings = function(dom, d) {
     var self = this;
 
-    // TODO
+    function refresh() {
+      // Graph meta:
+      var meta = d.get('graphMeta') || {};
+      $('*[data-tbn-settings-meta]', dom).each(function() {
+        var el = $(this);
+        el.val(meta[el.attr('data-tbn-settings-meta')]);
+      });
+
+      // Space data:
+      var space = d.get('space') || {};
+      $('*[data-tbn-settings-space]', dom).each(function() {
+        var el = $(this);
+        el.val(space[el.attr('data-tbn-settings-space')]);
+      });
+    }
+
+    refresh();
+
+    // Reference triggers:
+    this.triggers.events.graphMetaUpdated = refresh;
   };
 }).call(this);
