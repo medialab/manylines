2baf0b9774e93e76b48608f4fa2420373f4913f2
Alexis Jacomy <alexis.jacomy@gmail.com>
2014-05-21 16:36:40 +0200

    BFWIP: stuff

--- static/js/modules/app.basemap.js
+++ static/js/modules/app.basemap.js
@@ -46,6 +46,23 @@
       );
     });
 
+    // Columns layout
+    function openForcePanel() {
+      dom.filter('*[data-app-basemap-panel="sigma"]').removeClass('col-xs-9').addClass('col-xs-6');
+      dom.filter('*[data-app-basemap-panel="force"]').show();
+      $('.forcelayout-container .tirette', dom).hide();
+      renderer.resize();
+      renderer.render();
+    }
+
+    function closeForcePanel() {
+      dom.filter('*[data-app-basemap-panel="sigma"]').removeClass('col-xs-6').addClass('col-xs-9');
+      dom.filter('*[data-app-basemap-panel="force"]').hide();
+      $('.forcelayout-container .tirette', dom).show();
+      renderer.resize();
+      renderer.render();
+    }
+
     // Bind layout:
     $('*[data-app-basemap-action="startLayout"]', dom).click(function(e) {
       $('div[data-app-basemap-switchlayout]', dom).attr('data-app-basemap-switchlayout', 'on');
@@ -59,18 +76,15 @@
       e.preventDefault();
     });
 
-    // Columns layout
-    function openForcePanel() {
-      dom.filter('*[data-app-basemap-panel="sigma"]').removeClass('col-xs-9').addClass('col-xs-6');
-      dom.filter('*[data-app-basemap-panel="force"]').show();
-      renderer.resize();
-    }
-
-    function closeForcePanel() {
-      dom.filter('*[data-app-basemap-panel="sigma"]').removeClass('col-xs-6').addClass('col-xs-9');
-      dom.filter('*[data-app-basemap-panel="force"]').hide();
-      renderer.resize();
-    }
+    // Other buttons:
+    $('.forcelayout-container .tirette', dom).click(function(e) {
+      openForcePanel();
+      e.preventDefault();
+    });
+    dom.filter('*[data-app-basemap-panel="force"]').find('.tirette').click(function(e) {
+      closeForcePanel();
+      e.preventDefault();
+    });
 
     this.kill = function() {
       s.killForceAtlas2().killRenderer('tubemynet-basemap');
