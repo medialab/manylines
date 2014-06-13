40ad34c567e11028a7947289a12c647cf6c374ee
Yomguithereal <guillaumeplique@gmail.com>
2014-06-13 12:01:32 +0200

    Starting event binding

--- static/js/modules/app.views.js
+++ static/js/modules/app.views.js
@@ -6,6 +6,14 @@
     var self = this,
         sigmaController = new app.utils.sigmaController('views', dom, d);
 
+    // Binding actions
+    $('[data-app-view-action]', dom).click(function(e) {
+      var action = $(this).attr('data-app-view-action');
+      console.log(action);
+
+      e.preventDefault();
+    });
+
     this.kill = function() {
       sigmaController.killRenderer();
     };
