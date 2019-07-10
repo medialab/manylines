3db61f8da19ee0f9833b6ed17e386047b47008bb
Yomguithereal <guillaumeplique@gmail.com>
2019-07-10 16:30:57 +0200

    Static build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+echo "Cleanup..."
+rm -rf build
+mkdir build
+
+echo "Copying files..."
+cp -r static/* build/
+
+echo "Building HTML architecture..."
+cp build/site.html build/index.html
+mkdir build/app
+mkdir build/embed
+cp build/app.html build/app/index.html
+cp build/embed.html build/embed/index.html
